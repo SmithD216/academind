5894a8bb7111a7161ca0b98680f7d60d7036baec
spindleshanks <smithd216@protonmail.com>
2023-01-14 15:39:14 +0000

    Last lesson

---
+++ section11/operations.js
@@ -0,0 +1,20 @@
+//Math operations
+console.log(10+4);
+console.log(10-4);
+console.log(10/4);
+console.log(10*4);
+
+console.log(10%4);
+
+console.log(10 + 3 - 5.1 * 10)
+let result = 10 + 3 - 5.1 * 10;
+result++; //result = result + 1
+result--; //result = result - 1
+
+result += 5; //result = result + 5
+result -= 5; //result = result - 5
+result /= 5; //result = result / 5
+result *= 5; //result = result * 5
+
+let userName = "Max";
+console.log(userName.length);
