bba8ee5128f8a7da22541460d9f3bfcb6ac5a72c
spindleshanks <smithd216@protonmail.com>
2023-01-20 14:14:12 +0000

    Lesson 316

--- section14/scripts/app.js
+++ section14/scripts/app.js
@@ -1,4 +1,5 @@
 let editedPlayer = 0;
+let activePlayer = 0;
 
 const players = [
     {
@@ -15,12 +16,14 @@ const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
 const cancelConfigBtnElement = document.getElementById('cancel-config-btn');
 const startNewGameBtnElement = document.getElementById('start-game-btn');
+const gameFieldElements = document.querySelectorAll('#game-board li');
 
 const playerConfigOverlayElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
 const formElement = document.querySelector('form');
 const errorsOutputElement = document.getElementById('config-errors');
 const gameAreaElement = document.getElementById('active-game');
+const activePlayerNameElement = document.getElementById('active-player-name');
 
 
 
@@ -33,3 +36,7 @@ backdropElement.addEventListener('click',closePlayerConfig);
 formElement.addEventListener('submit', savePlayerConfig);
 
 startNewGameBtnElement.addEventListener('click', startNewGame);
+
+for (gameFieldElement of gameFieldElements){
+    gameFieldElement.addEventListener('click', selectGameField);
+}
