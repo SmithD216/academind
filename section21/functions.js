406187c8daf28c56fed5227196ee465f52d91e4e
spindleshanks <smithd216@protonmail.com>
2023-01-27 15:17:28 +0000

    lesson 384

---
+++ section21/functions.js
@@ -0,0 +1,21 @@
+function greetUser(greetingPrefix, userName = "user"){
+    console.log( greetingPrefix + " " + userName + "!");
+}
+
+greetUser('hi', 'DeeDee');
+greetUser('Hello');
+
+function sumUp(...numbers){
+    let result = 0
+    for(const number of numbers){
+        result += number
+    }
+
+    return result;
+}
+
+console.log(sumUp(1, 3,4, 5,6));
+
+const inputNumbers = [1, 5, 3, 5, 10]
+
+console.log(sumUp(...inputNumbers));
