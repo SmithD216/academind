ef502e7f28bfaa8400916dd62cce21945888ba6e
spindleshanks <smithd216@protonmail.com>
2023-01-27 11:04:33 +0000

    lesson 378

--- section20/app.js
+++ section20/app.js
@@ -1,11 +1,10 @@
-const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
-const uuid = require('uuid');
 const { get } = require('http');
 
-const resData = require('./util/restaurant-data');
+const defaultRoutes = require('./routes/default');
+const restaurantRoutes = require('./routes/restaurants');
 const restaurantData = require('./util/restaurant-data');
 
 const app = express();
@@ -16,55 +15,9 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', function (req, res) {
-  res.render('index');
-});
+app.use('/', defaultRoutes);
+app.use('/', restaurantRoutes);
 
-app.get('/restaurants', function (req, res) {
-  const storedRestaurants = resData.getStoredRestaurants();
-
-  res.render('restaurants', {
-    numberOfRestaurants: storedRestaurants.length,
-    restaurants: storedRestaurants,
-  });
-});
-
-app.get('/restaurants/:id', function (req, res) {
-  const restaurantId = req.params.id;
-
-  const storedRestaurants = resData.getStoredRestaurants();
-
-  for(const restaurant of storedRestaurants){
-    if(restaurant.id === restaurantId){
-      return res.render('restaurant-detail', {restaurant: restaurant});
-    }
-  };
-  res.status(404).render('404');
-});
-
-app.get('/recommend', function (req, res) {
-  res.render('recommend');
-});
-
-app.post('/recommend', function (req, res) {
-  const restaurant = req.body;
-  restaurant.id = uuid.v4();
-  const restaurants = resData.getStoredRestaurants();
-
-  restaurants.push(restaurant);
-
-  resData.storedRestaurants(restaurants);
-
-  res.redirect('/confirm');
-});
-
-app.get('/confirm', function (req, res) {
-  res.render('confirm');
-});
-
-app.get('/about', function (req, res) {
-  res.render('about');
-});
 
 app.use(function (req, res){
   res.status(404).render('404');
